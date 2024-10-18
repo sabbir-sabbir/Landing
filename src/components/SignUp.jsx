82c2e5fd4d7c5fda159038d49affbd86a47ac988
sabbir-sabbir <iosabbir7890@gmail.com>
2024-10-18 23:46:57 +0600

    new add

--- src/components/SignUp.jsx
+++ src/components/SignUp.jsx
@@ -4,9 +4,10 @@ import React from 'react'
 export default function SignUp() {
   return (
    
+    <>
     <div>
-
-        <h1 className="mt-[150px] text-center">This a SignUp page</h1>
+      <img src="no.jpg" alt="" />
     </div>
+    </>
   )
 }
